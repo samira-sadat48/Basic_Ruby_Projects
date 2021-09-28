0d985d85ac845292223fe3edfaf5aef647225ee2
samira-sadat48 <samira.sadat48@gmail.com>
2021-09-28 15:31:46 -0600

    Inital commit of substring script.

---
+++ Substrings/substrings.rb
@@ -0,0 +1,12 @@
+def substrings(word,dictionary)
+
+end
+
+#Main----------------------------------------------------
+print "Enter a word to check for: "
+word = gets.chomp
+
+#Test dictionary - replace with user input
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings(word,dictionary)
