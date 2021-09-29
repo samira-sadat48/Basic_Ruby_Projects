b739596c5901c5481bf4430582e89ab623aa2f7d
samira-sadat48 <samira.sadat48@gmail.com>
2021-09-29 10:07:46 -0600

    Hash with word count is printed to console

--- Substrings/substrings.rb
+++ Substrings/substrings.rb
@@ -1,19 +1,23 @@
-def substrings(word,dictionary)
-    #iterate through the dictionary
+def substrings(string,dictionary)
+    substrings_hash = Hash.new
+    lowercase_string = string.downcase
+
+    #iterate through the dictionary    
     dictionary.each do |currentWord|
-        #check if current word in dictionary of substring of user word
-        if word.include? currentWord
-            puts currentWord
+        #check if current word in dictionary of substring of user string
+        if lowercase_string.include? currentWord
+            #Add current word to hash (if it doesn't exist)
+            substrings_hash[currentWord] = lowercase_string.scan(currentWord).length
         end
     end
-     #True -> 
+    return substrings_hash
 end
 
 #Main----------------------------------------------------
-print "Enter a word to check for: "
-word = gets.chomp
+print "Enter a string: "
+string = gets.chomp
 
 #Test dictionary - replace with user input
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-puts substrings(word,dictionary)
+puts substrings(string,dictionary)
