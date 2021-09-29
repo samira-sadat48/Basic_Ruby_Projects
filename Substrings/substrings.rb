64b8101c226bb50788e50671a7a53a911bd035e4
samira-sadat48 <samira.sadat48@gmail.com>
2021-09-29 09:21:38 -0600

    Program iterates through the dictionary and correctly checks if substrings exist. Results are not stored yet.

--- Substrings/substrings.rb
+++ Substrings/substrings.rb
@@ -1,5 +1,12 @@
 def substrings(word,dictionary)
-
+    #iterate through the dictionary
+    dictionary.each do |currentWord|
+        #check if current word in dictionary of substring of user word
+        if word.include? currentWord
+            puts currentWord
+        end
+    end
+     #True -> 
 end
 
 #Main----------------------------------------------------
