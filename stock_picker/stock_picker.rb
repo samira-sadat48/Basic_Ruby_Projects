c3dc4a2f7ce6fc5fed58ba33e42f125a624b8d6e
samira-sadat48 <samira.sadat48@gmail.com>
2021-09-29 11:19:51 -0600

    Stock picker returns the buying and selling days using a profit comparison

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,8 +1,27 @@
 def stock_picker(stock_prices)
-    puts "test"
+    lowestPrice = stock_prices[0]
+    buyingDay = 0
+    profit = 0
+
+    tradingDays = [0,0]
+
+    stock_prices.each_with_index do |currentPrice, currentDay|
+        if currentPrice < lowestPrice
+            lowestPrice = currentPrice
+            buyingDay = currentDay
+            next
+        end
+
+        #profit check
+        if (currentPrice - lowestPrice) > profit
+            profit = currentPrice - lowestPrice
+            tradingDays = [buyingDay, currentDay]
+        end
+    end
+    tradingDays
 end
 
 #MAIN-----------------
-stock_prices_list = [17,3,6,9,15,8,6,1,10]
+stock_prices_list = [17, 3, 6, 9, 15, 8, 5, 1, 10]
 
-puts stock_picker(stock_prices_list)
+p stock_picker(stock_prices_list)
